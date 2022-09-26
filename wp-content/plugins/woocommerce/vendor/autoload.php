71641d71ea206e3a2552107d54adb3038e5dd650
simon <simonwong1985hk@gmail.com>
2022-09-26 12:52:13 +0800

    update woocommerce to 6.9.3

--- wp-content/plugins/woocommerce/vendor/autoload.php
+++ wp-content/plugins/woocommerce/vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2209626d89ea551641d9127876ba3fec::getLoader();
+return ComposerAutoloaderInit2b4c6a08e75c35cde9692cd7acbb163e::getLoader();
