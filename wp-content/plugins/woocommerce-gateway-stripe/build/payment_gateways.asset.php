b3db608808c458a4e4d1173aeb8b95c04483d5a6
Simon Wong <simonwong1985hk@gmail.com>
2023-02-20 19:15:06 +0800

    update woocommerce-gateway-stripe to 7.1.0

--- wp-content/plugins/woocommerce-gateway-stripe/build/payment_gateways.asset.php
+++ wp-content/plugins/woocommerce-gateway-stripe/build/payment_gateways.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-navigation', 'wp-components', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '86978df4ba69ff4f6bbc883acd3a1fcb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-navigation', 'wp-components', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'bc9c2cb82effc214b0a2710f73ed83d0');
